b4d1332d9d4897fdce10c0c21d271e6be14079ca
apissmr <114032956+apissmr@users.noreply.github.com>
2024-10-03 09:38:55 +0700

    Praktikum1JS3

---
+++ Praktikum1JS3.dart
@@ -0,0 +1,30 @@
+void main() {
+  // langkah 1
+  //  String test = "test2";
+  //  if (test == "test1") {
+  //     print("Test1");
+  //  } else if (test == "test2") {
+  //     print("Test2");
+  //  } else {
+  //     print("Something else");
+  //  }
+
+  //  if (test == "test2") print("Test2 again");
+
+  // Hasil =
+    //Test2
+    //Test2 again
+
+    //Exited.
+
+  //langkah 3
+    String test = "true";
+    if (test == "true") {
+      print("Kebenaran");
+    }
+  // Hasil = 
+  // Connected to the VM Service.
+  // Kebenaran
+
+  // Exited.
+}
